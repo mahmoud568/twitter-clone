07d4981a27f04362fde7f1a40a6a9cad04a8167c
mahmoud abdelwahab <mahmoudabdelwahab4242@gmail.com>
2023-05-13 09:25:47 +0300

    restructure my app folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { TimelineForyouComponent } from './pages/home/timeline-foryou/timeline-foryou.component';
-import { TimelineFollowingComponent } from './pages/home/timeline-following/timeline-following.component';
-import { TweetComponent } from './pages/home/tweet/tweet.component';
-import { SearchComponent } from './pages/side-taps/search/search.component';
-import { MiniTagsComponent } from './pages/side-taps/mini-tags/mini-tags.component';
-import { MiniWhoToFollowComponent } from './pages/side-taps/mini-who-to-follow/mini-who-to-follow.component';
-import { SideTapsComponent } from './pages/side-taps/side-taps.component';
-import { HeaderComponent } from './pages/header/header.component';
+import { TweetComponent } from './shared/components/tweet/tweet.component';
+import { HomeComponent } from './features/home/home.component';
+import { MiniTagsComponent } from './features/side-taps/mini-tags/mini-tags.component';
+import { MiniWhoToFollowComponent } from './features/side-taps/mini-who-to-follow/mini-who-to-follow.component';
+import { TimelineForyouComponent } from './features/home/timeline-foryou/timeline-foryou.component';
+import { TimelineFollowingComponent } from './features/home/timeline-following/timeline-following.component';
+import { SideTapsComponent } from './features/side-taps/side-taps.component';
+import { NavigationComponent } from './shared/components/navigation/navigation.component';
+import { SearchBarComponent } from './shared/components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SearchComponent,
     MiniTagsComponent,
     MiniWhoToFollowComponent,
     TimelineForyouComponent,
     TimelineFollowingComponent,
     TweetComponent,
     SideTapsComponent,
-    HeaderComponent
+    NavigationComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/shared/components/search-bar/search-bar.component.spec.ts
+++ src/app/shared/components/search-bar/search-bar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SearchComponent } from './search.component';
+import { SearchBarComponent } from './search-bar.component';
 
-describe('SearchComponent', () => {
-  let component: SearchComponent;
-  let fixture: ComponentFixture<SearchComponent>;
+describe('SearchBarComponent', () => {
+  let component: SearchBarComponent;
+  let fixture: ComponentFixture<SearchBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [ SearchBarComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(SearchComponent);
+    fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
